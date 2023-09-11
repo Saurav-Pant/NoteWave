c38be94fe4c1f3e6a6ecd507949b28d48c817734
Saurav Pant <pantsaurav005@gmail.com>
2023-09-11 23:54:32 +0530

    Backend Register also Done

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
-
-const handleSubmit=(e)=>{
+const handleSubmit = (e: any) => {
   e.preventDefault();
 
-}
+  fetch("api/users/Login");
+};
 
 const RegisterPage = () => {
+  const [UserName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-900 to-black relative">
       <button className="bg-red-800 text-white absolute top-10 left-10 px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none">
@@ -25,6 +29,9 @@ const RegisterPage = () => {
               id="name"
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
               placeholder="Your name"
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
             />
           </div>
           <div className="mb-4">
@@ -36,6 +43,9 @@ const RegisterPage = () => {
               id="email"
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
               placeholder="Your email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
           </div>
           <div className="mb-4">
@@ -47,13 +57,17 @@ const RegisterPage = () => {
               id="password"
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
               placeholder="Your password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
           <button
             type="submit"
             className="w-full py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none"
+            onSubmit={handleSubmit}
           >
-            Sign Up
+            Register
           </button>
         </form>
         <div className="mt-4">
