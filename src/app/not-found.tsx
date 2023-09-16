c20b2cb5b7f66aca2ad612110e9948954a40a790
Saurav Pant <pantsaurav005@gmail.com>
2023-09-16 21:52:53 +0530

    animated ping

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-l from-slate-800 to bg-purple-900">
       <div className="text-center">
-        <h1 className="text-5xl font-bold mb-4 bg-gradient-to-tl from bg-slate-200 to bg-white text-transparent bg-clip-text">
+        <h1 className="text-5xl font-bold mb-4 bg-gradient-to-tl from bg-slate-200 to bg-white text-transparent bg-clip-text animate-ping">
           404 Not Found
         </h1>
         <button className="p-2 mt-6 bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 text-white rounded-md">
