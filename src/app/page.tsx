407419782f7d81660972d2a14653481c00c2cbd5
Saurav Pant <pantsaurav005@gmail.com>
2023-11-23 08:46:27 +0530

    Home page some bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,22 +10,22 @@ type Props = {};
 
 const Page = (props: Props) => {
   const token = Cookies.get("token");
-  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  const { userId, sessionId } = useAuth();
 
 
   return (
-    <div className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black h-screen">
+    <div className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black h-screen w-full">
       <Navbar />
       <div className="flex flex-col items-center justify-center h-[70vh] text-white">
-        <div className="text-6xl font-semibold mb-4 text-center">
+        <div className="text-3xl sm:text-6xl font-semibold mb-4 text-center">
           <span
-            className="bg-gradient-to-tl from-slate-200 via-gray-400 to-white text-transparent bg-clip-text"
+            className="bg-gradient-to-tl from-slate-200 via-gray-400 to-white text-transparent bg-clip-text "
             style={{ WebkitBackgroundClip: "text" }}
           >
             Share Knowledge With NoteWave
           </span>
         </div>
-        <div className="text-lg opacity-75 text-center">
+        <div className="text-sm sm:text-lg opacity-75 text-center">
           Cultivate knowledge-sharing excellence with NoteWave, where notes
           converge, fostering <br /> discovery and collaboration. Elevate your
           learning experience today
