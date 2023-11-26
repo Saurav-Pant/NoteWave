e3c15d22f4db563efbe4cedc6966a5f58ad26763
Saurav Pant <pantsaurav005@gmail.com>
2023-11-26 18:53:38 +0530

    Final Checking

--- src/db/appwrite.js
+++
@@ -1,8 +0,0 @@
-import { Client, Storage, ID, Databases } from "appwrite";
-
-export const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("64fab385a91bbfe6074d");
-
-export const storage = new Storage(client);
-export const databases = new Databases(client);
