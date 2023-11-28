e6aa3dc93a321dacab42f3bc049c9cd5dcd2e94c
Saurav Pant <pantsaurav005@gmail.com>
2023-11-28 23:36:49 +0530

    Fonts added

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
+import Back from "@/components/Back";
 
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-400 via-gray-600 to-gray-800">
+      <Back/>
+      <div>
       <SignIn />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 import { UserButton } from "@clerk/nextjs";
 import { useAuth } from "@clerk/nextjs";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const router = useRouter();
@@ -18,28 +19,36 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center p-4 border-b-slate-50 border-b-2">
-      <div className="text-white text-xl font-bold">
-        <Link href="/">NoteWave</Link>
-      </div>
+      <motion.div
+        className="text-white"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        <Link href="/" className="text-2xl font-bold">
+          NoteWave
+        </Link>
+      </motion.div>
+
       <div className="flex space-x-4 justify-between items-center">
         {userId ? (
-         <div className="flex items-center">
-         <button className="bg-white text-black font-semibold py-2 px-4 rounded-full hover:bg-slate-300 transition-colors duration-500 ease-in-out mr-5">
-           <Link href="/Upload">Upload</Link>
-         </button>
-       
-         {token ? (
-           <button
-             className="bg-white text-black font-semibold py-2 px-4 rounded-full hover:bg-slate-300 transition-colors duration-500 ease-in mr-5"
-             onClick={handleLogout}
-           >
-             Logout
-           </button>
-         ) : null}
-       
-         <div className="">{userId ? <UserButton afterSignOutUrl="/" /> : null}</div>
-       </div>
-       
+          <div className="flex items-center">
+            <button className="bg-white text-black font-semibold py-2 px-4 rounded-full hover:bg-slate-300 transition-colors duration-500 ease-in-out mr-5">
+              <Link href="/Upload">Upload</Link>
+            </button>
+
+            {token ? (
+              <button
+                className="bg-white text-black font-semibold py-2 px-4 rounded-full hover:bg-slate-300 transition-colors duration-500 ease-in mr-5"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            ) : null}
+
+            <div className="">
+              {userId ? <UserButton afterSignOutUrl="/" /> : null}
+            </div>
+          </div>
         ) : (
           <div>
             <button className="bg-white  text-black font-semibold py-2 px-4 rounded-full hover:bg-slate-300 transition-colors duration-500 ease-in-out">
