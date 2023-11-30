eaafb21360a688a18b3525352b8cd05590a3d200
Saurav Pant <pantsaurav005@gmail.com>
2023-11-30 11:15:01 +0530

    Middleware done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const cookie = request.cookies.get("token");
   const __session = request.cookies.get("__session");
-  console.log(cookie);
-  console.log(__session);
   if (!__session?.value) {
     return NextResponse.redirect(new URL("/Login", request.url));
   }
