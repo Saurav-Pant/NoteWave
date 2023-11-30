87bbe98b02a3d4e1fe73e09d97e61372b85a5b5b
Saurav Pant <pantsaurav005@gmail.com>
2023-11-30 10:43:31 +0530

    Update Topic UI changed

--- src/components/EditTopicForm.tsx
+++ src/components/EditTopicForm.tsx
@@ -3,13 +3,13 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function EditTopicForm({ id, title, description }:any) {
+export default function EditTopicForm({ id, title, description }: any) {
   const [newTitle, setNewTitle] = useState(title);
   const [newDescription, setNewDescription] = useState(description);
 
   const router = useRouter();
 
-  const handleSubmit = async (e:any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     try {
@@ -33,26 +33,30 @@ export default function EditTopicForm({ id, title, description }:any) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      <input
-        onChange={(e) => setNewTitle(e.target.value)}
-        value={newTitle}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Title"
-      />
+    <div className="flex justify-center items-center h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black ">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 p-8 bg-white rounded-md shadow-md"
+      >
+        <input
+          onChange={(e) => setNewTitle(e.target.value)}
+          value={newTitle}
+          className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          type="text"
+          placeholder="Topic Title"
+        />
 
-      <input
-        onChange={(e) => setNewDescription(e.target.value)}
-        value={newDescription}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Description"
-      />
+        <textarea
+          onChange={(e) => setNewDescription(e.target.value)}
+          value={newDescription}
+          className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          placeholder="Topic Description"
+        />
 
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
-        Update Topic
-      </button>
-    </form>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:border-blue-300">
+          Update Topic
+        </button>
+      </form>
+    </div>
   );
 }
