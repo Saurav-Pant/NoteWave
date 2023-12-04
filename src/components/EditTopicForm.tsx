a26a974595430ce1de4eaddb6b1fc04c1033e2ef
Saurav Pant <pantsaurav005@gmail.com>
2023-12-04 16:47:32 +0530

    Now done

--- src/components/EditTopicForm.tsx
+++ src/components/EditTopicForm.tsx
@@ -14,7 +14,7 @@ export default function EditTopicForm({ id, title, description }: any) {
     e.preventDefault();
 
     try {
-      const res = await fetch(`http://localhost:3000/api/upload/${id}`, {
+      const res = await fetch(`https://notesswave.vercel.app/api/upload/${id}`, {
         method: "PUT",
         headers: {
           "Content-type": "application/json",
